f27675bff460d0b144579226b3b5565b14d3bcce
Evangel Teo <evangelteo@gmail.com>
2018-05-18 16:22:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function addElementToBeginningOfArray(array,element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array.unshift(element)
+  return array.unshift('element')
 }
 
 function addElementToEndOfArray(array,element) {
@@ -25,7 +25,7 @@ function addElementToEndOfArray(array,element) {
 }
 
 function destructivelyAddElementToEndOfArray(array,element) {
-  return array.push(element)
+  return array.push('element')
 }
 
 function accessElementInArray(array,index) {
